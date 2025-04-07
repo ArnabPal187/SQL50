e020d679d0c51e2cd05c8aaebc75e3b619fa4577
ArnabPal187 <77872764+ArnabPal187@users.noreply.github.com>
2025-04-08 01:57:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (84.62%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,8 +1,16 @@
+from functools import reduce
+
 class Solution:
     def canPartition(self, nums: list[int]) -> bool:
-
         target, isOdd = divmod(sum(nums), 2)
-        if isOdd: return False
-
-        bitmap = reduce(lambda x, y: x|(x << y), nums, 1)
+        if isOdd:
+            return False
+        
+        # Create the bitmap to track possible subset sums
+        bitmap = 1  # Start with 1, representing the sum of 0
+        for num in nums:
+            # Update the bitmap by considering the current num
+            bitmap |= bitmap << num
+        
+        # Check if we can form the 'target' sum
         return bitmap & (1 << target) != 0
